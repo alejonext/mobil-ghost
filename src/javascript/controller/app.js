1d3a2aca060c6502c3c24fd11d4efbe0e16b88fe
pol <Alejo Next>
2014-12-05 14:47:03 -0500

    Add controller app

--- src/javascript/controller/app.js
+++ src/javascript/controller/app.js
@@ -1,1 +1,26 @@
-﻿
+﻿module.exports = function (scope, stroge, location ){
+	scope.is = stroge.$default({
+		login : false,
+		local : location.path(),
+		url : ''
+	});
+
+	if(!scope.is.login && location.path().split('/')[1] != 'login' ){
+		location.path('#/login');
+		stroge.reset();
+	}
+
+	scope.loginOut = function () {
+		location.path('#/login');
+		stroge.reset();
+	};
+
+	scope.is.local = location.path();
+
+};
+
+module.exports.$inject = [
+	'$scope',
+	'$localStorage',
+	'$location'
+];
